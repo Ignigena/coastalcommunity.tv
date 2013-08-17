b386eb2c0000def79a6d64500e924bedccfbcdba
Albert Martin <albert@imor.tl>
2013-08-17 16:10:31 +0000

    Service times block moved to code.

---
+++ sites/all/modules/coastal/service_times_block/service_times.tpl.php
@@ -0,0 +1,7 @@
+<div class="maxwidth">
+	<a class="get-directions" href="#">Get Directions</a>
+	Sundays @ <strong>9:15AM</strong> + <strong>11:15AM</strong>
+	<span class="social">
+		<a class="facebook" href="https://www.facebook.com/CoastalCommunityChurch" target="_blank">f </a><a class="twitter" href="https://twitter.com/coastalchurch" target="_blank">t</a><a class="youtube" href="http://www.youtube.com/user/coastalccfl" target="_blank">U</a><a class="googleplus" href="https://plus.google.com/115286991291180557180" target="_blank">+</a>
+	</span>
+</div>

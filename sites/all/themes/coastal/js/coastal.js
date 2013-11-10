23340eef2d9e629739ae09a6e2ec39c5d1556991
Albert Martin <albertmartin@mac.com>
2013-11-10 23:07:51 +0000

    Fix grey screen of death on Leaflet.

--- sites/all/themes/coastal/js/coastal.js
+++ sites/all/themes/coastal/js/coastal.js
@@ -1,7 +1,9 @@
 (function ($) {
   $(document).ready(function($) {
     $('#block-views-location-block a.get-directions').click(function() {
-      $('#block-views-location-block .leaflet-container').slideToggle(600);
+      $('#block-views-location-block .leaflet-container').slideToggle(400, function() {
+        _leaflet_resize3();
+      });
       return false;
     });
   });

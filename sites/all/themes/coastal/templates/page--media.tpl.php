47070821362a13a9dcbe37545314c2295444d415
Albert Martin <albertmartin@mac.com>
2013-11-28 19:13:20 +0000

    Fixed issue with Omega theme updating inside itself.

---
+++ sites/all/themes/coastal/templates/page--media.tpl.php
@@ -0,0 +1,82 @@
+<header id="header" role="banner">
+  <div class="container">
+    <?php print render($page['header_top']); ?>
+  
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+    <?php endif; ?>
+  
+    <?php if ($site_name || $site_slogan): ?>
+      <hgroup id="name-and-slogan">
+        <?php if ($site_slogan): ?>
+          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
+        <?php endif; ?>
+      </hgroup>
+    <?php endif; ?>
+  
+    <?php print render($page['header']); ?>
+  </div>
+</header>
+
+<div id="main">
+  <div class="titlewrap">
+    <div class="container">
+      <?php print render($tabs); ?>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+    </div>
+  </div>
+
+  <div id="main-content" role="main" class="container">
+    <?php print render($page['highlighted']); ?>
+    <a id="main-content-anchor"></a>
+    <?php print render($title_suffix); ?>
+    <div class="row-fluid">
+      <?php
+        $spancount = 12;
+        if (render($page['sidebar_first'])):
+          $spancount -= 4;
+      ?>
+      <div class="sidebar span4">
+        <div data-spy="affix" data-offset-top="200">
+          <?php print render($page['sidebar_first']); ?>
+        </div>
+      </div>
+      <?php
+        endif;
+        
+        if (render($page['sidebar_second'])) $spancount -= 4;
+      ?>
+      <div class="span<?php print $spancount; ?>">
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php print $messages; ?>
+        <?php print render($page['help']); ?>
+        <?php print render($page['content']); ?>
+      </div>
+      <?php if (render($page['sidebar_second'])): ?>
+      <div class="sidebar span4">
+        <?php print render($page['sidebar_second']); ?>
+      </div>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
+<div id="below">
+  <div id="below-content" class="container">
+    <div class="row-fluid">
+      <?php print render($page['below_content']); ?>
+    </div>
+  </div>
+</div>
+<?php if (render($page['navigation'])): ?>
+  <div id="navigation">
+    <?php print render($page['navigation']); ?>
+  </div>
+<?php endif; ?>
+
+<?php print render($page['footer']); ?>
+<?php print render($page['bottom']); ?>

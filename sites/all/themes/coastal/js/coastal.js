60258bf2fcbb112653b0aeb47edcab8634c40678
Albert Martin <albert@bethel.io>
2013-12-12 05:20:11 +0000

    Added video playback abilities in header of page.

--- sites/all/themes/coastal/js/coastal.js
+++ sites/all/themes/coastal/js/coastal.js
@@ -6,5 +6,13 @@
       });
       return false;
     });
+    
+    if ($('article.node--page--full .field--name-field-video iframe').length) {
+      $('article.node--page--full .field--name-field-video iframe').hide();
+      $('article.node--page--full .field--name-field-image').addClass('video-player');
+      $('article.node--page--full .field--name-field-image').click(function() {
+        $('article.node--page--full .field--name-field-video iframe').fadeIn(300);
+      });
+    }
   });
 })(jQuery);

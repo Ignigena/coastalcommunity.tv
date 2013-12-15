43f28d7bfb56bc209b7c98643149b72397792eff
Albert Martin <albert@bethel.io>
2013-12-15 20:35:47 +0000

    Added Giving Login block.

---
+++ sites/all/modules/coastal/giving_login/theme/giving_login.tpl.php
@@ -0,0 +1,9 @@
+<form action="https://coastcfl.infellowship.com/UserLogin/Attempt?returnurl=%2Fonlinegiving" method="post" target="_blank">
+  <div class="box">
+    <p><label for="username">Login</label><br><input class="huge input_full" id="username" name="username" type="text" value=""></p>
+    <p><label for="password">Password</label><br><input class="huge input_full" id="password" name="password" type="password"></p>
+    <p><input id="rememberme" name="rememberme" type="checkbox" value="true"><input name="rememberme" type="hidden" value="false"><br><label for="rememberme">Remember Me</label></p>
+    <p><span class="submit_wait"><input type="submit" id="btn_login" name="btn_login" value="Sign in"></span></p>
+    <p>Don't have an account? <a href="https://coastcfl.infellowship.com/UserLogin/New">Sign up!</a></p>
+  </div>
+</form>

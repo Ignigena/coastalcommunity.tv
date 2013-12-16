21ff50fc12014a9bb6ec0a21c17dfa1263d7c51f
Albert Martin <albert@bethel.io>
2013-12-16 23:02:05 +0000

    Modified Generator meta-tag.

--- sites/all/themes/coastal/template.php
+++ sites/all/themes/coastal/template.php
@@ -5,7 +5,11 @@
   }
 
   require_once __DIR__ . '/includes/form.inc';
-  
+
   function coastal_preprocess_html(&$variables) {
     drupal_add_js(drupal_get_path('theme', 'coastal') . '/js/coastal.js', array('group' => JS_THEME));
+  }
+
+  function coastal_html_head_alter(&$head_elements) {
+    $head_elements['metatag_generator']['#value'] = 'Bethel (http://bethel.io)';
   }

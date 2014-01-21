b2777835d764424586b3bddad6f00f12379c09fe
Albert Martin <albert@bethel.io>
2014-01-21 01:17:53 +0000

    Fixed Online Giving login box errors.

--- sites/all/modules/coastal/giving_login/theme/giving_login.tpl.php
+++ sites/all/modules/coastal/giving_login/theme/giving_login.tpl.php
@@ -8,6 +8,7 @@
       <input class="form-control form-text" placeholder="Password" type="password" id="password" name="password" size="60" maxlength="128">
     </div>
     <div>
+      <input name="rememberme" type="hidden" value="false">
       <input class="btn btn-lg btn-primary btn-block button form-submit" type="submit" id="btn_login" name="btn_login" value="Log in">
       <p class="muted">Don't have an account? <a href="https://coastcfl.infellowship.com/UserLogin/New">Sign up!</a></p>
     </div>

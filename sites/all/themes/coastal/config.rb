ac4d109f0f9556ab52feab0e5b37f03b8da6f079
Albert Martin <albert@bethel.io>
2014-02-06 23:42:29 +0000

    Started SASS conversion for theme stylesheets.

---
+++ sites/all/themes/coastal/config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+preferred_syntax = :scss
+
+output_style = :compressed # by Compass.app 
+line_comments = false # by Compass.app 
